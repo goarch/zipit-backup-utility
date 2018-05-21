c03da5f61c5cfdf81408c84f87c1b219795236d2
Niko Kyritsis <nkyritsis@goarch.org>
2018-05-21 16:17:09 -0400

    Update Full-Backup-Default-profile.php
    Updated Full-Backup-Default profile to exclude zipit directory

--- excludes/Full-Backup-Default-profile.php
+++ excludes/Full-Backup-Default-profile.php
@@ -1,5 +1,5 @@
 <?php
 // lib and cgi-bin are excluded by default due to issues on PHP 5.6
 $file_excludes = "";
-$folder_excludes = "./lib\* ./web/cgi-bin\*";
+$folder_excludes = "./logs\* ./web/content/zipit\* ./lib\* ./web/cgi-bin\*";
 ?>
