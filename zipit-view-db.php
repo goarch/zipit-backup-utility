c77c10f9a07e5897f8f0ea61de68a77103072c74
Niko Kyritsis <nkyritsis@goarch.org>
2018-07-16 15:03:27 -0400

    Remove zipit from Container name

--- zipit-view-db.php
+++ zipit-view-db.php
@@ -45,7 +45,7 @@ catch (HttpUnauthorizedError $e) {
 
 // create container if it doesn't already exist
 $cont = $ostore->Container();
-$cont->Create(array('name'=>"zipit-backups-databases-$url"));
+$cont->Create(array('name'=>"$url"));
 
 $list = $cont->ObjectList();
 
